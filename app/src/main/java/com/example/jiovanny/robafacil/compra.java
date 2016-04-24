b93be3bd6586b1fa456b05ca9426e1dd71c0cf92
danieldec <danieldelrioc@gmail.com>
2016-04-24 13:08:02 -0600

    misc.xml y compra.java

--- app/src/main/java/com/example/jiovanny/robafacil/compra.java
+++ app/src/main/java/com/example/jiovanny/robafacil/compra.java
@@ -72,7 +72,7 @@ public class compra extends AppCompatActivity {
             }
         });
     }
-    
+
     public void cargarSQLiteProducto(){
         Producto productoB=mydb.getProducto(consultaBus);
         Toast.makeText(getApplicationContext(),"Este es el query "+ consultaBus +" Categoria "+ categoriaBus,Toast.LENGTH_SHORT).show();
